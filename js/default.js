84c417c614ac1caf032f5ac3e3556af44fdbde02
liyanguestc <liyanguestc@gmail.com>
2015-02-10 14:10:55 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -7,14 +7,15 @@ var material;
 var MeshHH, GeoMM, GeoAM;
 var bInitTimeObject = false;
 window.onload = function () {
-  Init();
-  animate();
+  LEIA.physicalScreen.InitFromExternalJson('https://s3.amazonaws.com/leiacore/config.json',function(){
+    Init();
+    animate();
+  });
 };
 
 function Init() {
   LEIA.virtualScreen.Init();
-  LEIA.physicalScreen.InitFromExternalJson('https://s3.amazonaws.com/leiacore/config.json');
-  LEIA.physicalScreen.resolution = new THREE.Vector2(200,150);
+ 
   scene = new THREE.Scene();
   group = new THREE.Object3D();
 
